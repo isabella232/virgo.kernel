086315da9a39e501d9b087610fdddfc26658d1e2
Florian Waibel <fwaibel@eclipsesource.com>
2014-12-16 22:21:02 +0100

    Switches back from OSGi plugin to Bundlor

---
+++ org.eclipse.virgo.shell.command/src/main/java/org/eclipse/virgo/shell/internal/BundlorGradlePluginDoesntLookIntoXmlFiles.java
@@ -0,0 +1,9 @@
+package org.eclipse.virgo.shell.internal;
+
+import org.eclipse.virgo.medic.eventlog.EventLogger;
+
+// TODO - remove once the Gradle Plugin is enhanced
+public class BundlorGradlePluginDoesntLookIntoXmlFiles {
+
+    EventLogger eventLogger;
+}
