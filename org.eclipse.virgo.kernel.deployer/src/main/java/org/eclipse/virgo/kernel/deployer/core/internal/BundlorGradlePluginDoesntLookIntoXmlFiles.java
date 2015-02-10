d64981d05153e339aabdd86bdc23513f34fd1b35
Florian Waibel <fwaibel@eclipsesource.com>
2015-02-10 20:27:54 +0100

    Fixes Manifest generation for oev.management.console

--- org.eclipse.virgo.kernel.deployer/src/main/java/org/eclipse/virgo/kernel/deployer/core/internal/BundlorGradlePluginDoesntLookIntoXmlFiles.java
+++ org.eclipse.virgo.kernel.deployer/src/main/java/org/eclipse/virgo/kernel/deployer/core/internal/BundlorGradlePluginDoesntLookIntoXmlFiles.java
@@ -1,8 +1,10 @@
 package org.eclipse.virgo.kernel.deployer.core.internal;
 
 import org.eclipse.virgo.kernel.services.work.WorkArea;
+import org.eclipse.virgo.nano.serviceability.dump.DumpCoordinator;
 
 public class BundlorGradlePluginDoesntLookIntoXmlFiles {
 
     private WorkArea workArea;
+    private DumpCoordinator dumpCoordinator;
 }
